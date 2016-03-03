281a392ae7fb5c9a7ef5beaafe0fc1d032b60c94
tomaswolfgang <tomaswolfgang@gmail.com>
2016-03-03 13:28:14 -0500

    Scripts
    Contains all of my base scripts (including base player, base item)
    Contains camera movement scripts
    Contains player map  travelling scripts
    contains saving and loading between scenes scripts

---
+++ Scripts/IncreaseExperience.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class IncreaseExperience {
+
+    private static int xpToGive;
+
+    
+
+    public static void AddExperience()
+    {
+        xpToGive = GameInformation.PlayerLevel * 100;
+        GameInformation.CurrentXP += xpToGive;
+        CheckLevelUp();
+    }
+
+    private static void CheckLevelUp()
+    {
+        if (GameInformation.CurrentXP >= GameInformation.RequiredXP)
+        {
+            Debug.Log("You've leveled up!");
+            //create level up script
+        }
+    }
+	
+}
